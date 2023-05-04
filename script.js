ab2ae6f2c8214c63cd7ac74409ef70ce6fb125e2
jmsmith23 <smitjeff23@gmail.com>
2023-05-03 21:10:34 -0500

    added classes to game board clue boxes and category boxes. Added event listener to board. Began creating a modal

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-	/*----- constants -----*/
-    const players =  {
-        one: 'playerOne',
-        two: 'playerTwo'
-    }
-    
-        /*----- state variables -----*/
-    let turn;
-    let board;
-    let winner;
-    
-        /*----- cached elements  -----*/
-    
-    const gameBoard = document.getElementById('game-board')
-    const p1Score = document.getElementById('p1-score')
-    const p2Score = document.getElementById('p2-score')
-    
-        /*----- event listeners -----*/
-    
-    
-        /*----- functions -----*/
 
-    function createGameBoard() {
-        for (let i = 0; i < 30; i++) {
-            let box = document.createElement('div')
+/*----- constants -----*/
+const players =  {
+    one: 'playerOne',
+    two: 'playerTwo'
+}
+
+/*----- state variables -----*/
+let turn;
+let board;
+let winner;
+
+/*----- cached elements  -----*/
+
+const gameBoard = document.getElementById('game-board')
+const p1Score = document.getElementById('p1-score')
+const p2Score = document.getElementById('p2-score')
+// const clueBox = document.getElementsByClassName('board-box')
+const modal = document.getElementById('questionModal')
+/*----- event listeners -----*/
+
+
+
+/*----- functions -----*/
+
+// Creates game board dynamically
+function createGameBoard() {
+    for (let i = 1; i <= 30; i++) {
+        let box = document.createElement('div')
+        if (i <= 5) {
+            box.className = 'category'
+        } else {
             box.className = 'board-box'
-            gameBoard.appendChild(box)
         }
+        gameBoard.appendChild(box)
     }
-    createGameBoard()
+}
+
+createGameBoard()
+
+const clueBoxes = document.querySelectorAll('.board-box')
+
+clueBoxes.forEach((box) => {
+    box.addEventListener('click', () => {
+        alert('Hello')
+    })
+})
 
 
 
-   
